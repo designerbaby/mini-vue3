ff4b986905e92ebae2fb54abf9b2282d0bd8cf05
liujiana <liujiana@yy.com>
2020-09-19 15:25:39 +0800

    feat: 补全reactivity

--- reactivity/README.md
+++ reactivity/README.md
@@ -1,3 +1,16 @@
-reactive: reactive({})
-原理：
-  借助es6的proxy的set和get,实现响应式
+
+### 1、作用：
+  - reactive: reactive({}) // 可以将对象转换为proxy响应式对象
+  - computed(() => {}) // 和vue2中相同，计算属性
+  - effect() // 直接去执行
+### 2、原理：
+  使用es6自带的属性，每个属性有get和set方法，借助es6的proxy,进行拦截。
+### 3、思路：
+  用map收集所有依赖：
+  ```
+  { // targetMap: 
+    target : { // depMap
+      key: [effect] // dep
+    }
+  }
+  ```
--- reactivity/reactivity.js
+++ reactivity/reactivity.js
@@ -1,15 +1,65 @@
-import { track } from "@vue/reactivity"
+
+// 用map收集所有依赖
+// { // targetMap: 
+//   target : { // depMap
+//     key: [effect] // dep
+//   }
+// }
 
 let targetMap = new WeakMap()
-let effectStack = []
+let effectStack = [] // 存储effect
 function track(target, key) {
-  const effect = effectStack
+  // 初始化
+  const effect = effectStack[effectStack.length - 1]
+  if (effect) {
+    let depMap = targetMap.get(target)
+    if (depMap === undefined) { // 如果内部没有对象,就初始化一个
+      depMap = new Map()
+      targetMap.set(target, depMap)
+    }
+
+    let dep = depMap.get(key)
+    if (dep === undefined) { // 如果内部没有deps
+      dep = new Set()
+      depMap.set(key, dep)
+    }
+
+    // 双向缓存
+    if (!dep.has(effect)) { // 如果里面没有effect
+      dep.add(effect)
+      effect.deps.push(dep)
+    }
+  }
 }
+
+function trigger(target, key, info) {
+  let depMap = targetMap.get(target)
+  if (depMap === undefined) {
+    return  // 没有effect
+  }
+  const effects = new Set()
+  const computeds = new Set() // computed是一个特殊的effect
+
+  if (key) {
+    let deps = depMap.get(key)
+    deps.forEach(effect => {
+      if (effect.computed) {
+        computeds.add(effect)
+      } else {
+        effects.add(effect)
+      }
+    })
+  }
+
+  effects.forEach(effect => effect())
+  computeds.forEach(computed => computed())
+}
+
 const baseHandler = {
   get(target, key) {
     const ret = target[key]
     track(target, key)
-    return ret
+    return ret // typeof ret === 'object'? reactive(ret): ret
   },
   set(target, key, value) {
     const info = {
@@ -17,18 +67,51 @@ const baseHandler = {
       newValue: value
     }
     target[key] = value
+    trigger(target, key, info)
   }
 }
 
-function reactive() {
+function reactive(target) {
+  // debugger
   const observed = new Proxy(target, baseHandler)
   return observed
 }
 
-function computed() {
-
+function computed(fn) {
+  const runner = effect(fn, { computed: true, lazy: true })
+  return {
+    effect: runner,
+    get value() {
+      return runner()
+    }
+  }
 }
 
-function effect() {
+function effect(fn, options = {}) {
+  let e = createReactiveEvent(fn, options)
+  if (!options.lazy) {
+    e()
+  }
+  return e
+}
 
+function createReactiveEvent(fn, options) {
+  let effectInner = function effectInner(...args) {
+    return run(effectInner, fn, args)
+  }
+  effectInner.deps = []
+  effectInner.computed = options.computed
+  effectInner.lazy = options.lazy
+  return effectInner
+}
+
+function run (aEffect, fn, args) {
+  if (effectStack.indexOf(aEffect) === -1) { // 里面没有这个effect
+    try {
+      effectStack.push(aEffect)
+      return fn(...args)
+    } finally {
+      effectStack.pop()
+    }
+  }
 }
